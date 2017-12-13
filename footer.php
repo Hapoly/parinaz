07c405421c3a0288f698368c78e5727ba6ff3f07
mr-exception <drabandi1996@gmail.com>
2017-12-13 06:12:39 +0330

    header and footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+      <div class="footer">
+        <p>design by <a href="https://github.com/Hapoly">hapoly</a></p>
+      </div>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <a class="navbar-brand " href="#" style="text-align:center">WebSiteName</a>
+            </div>
+            <form class="navbar-form navbar-right">
+            <button type="submit" class="btn btn-default">new</button>
+        </div>
+    </nav>
