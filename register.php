3644eb4388e99732ba341cfa6bc7828a5426adfc
sadafN <n.shell120@gmail.com>
2017-12-14 17:26:41 +0330

    changed again!

--- register.php
+++ register.php
@@ -16,67 +16,47 @@
     </nav>
 <div class="container" style="margin-top:50px;">
 <div class="row">
-<div class="col-md-6 col-md-offset-3">
+<div class="col-md-8 col-md-offset-2">
  <div class="panel panel-default text-center">
     <div class="panel-body">
-        <div class="container">
-                <form >
-                        <div class="form-group">
-                          <label for="email">Email:</label>
-                          <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-                        </div>
-                        <div class="form-group">
-                          <label for="pwd">Password:</label>
-                          <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
-                        </div>
-                        <div class="checkbox">
-                          <label><input type="checkbox" name="remember"> Remember me</label>
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
-                      </form>              
+        <form class="form-horizontal" >
+            <div class="form-group">
+                <div class="row">
+                <label class="control-label col-sm-2" for="email" style="float:left;">Email:</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                 </div>
-                  
-        <?php 
-            if(isset($_POST['register'])){
-
-                $db_servername = "localhost";
-                $db_username = "root";
-                $db_password = "sadaf";
-                $db_dbname = "panel";
-
-
-
-                try {
-                    $conn = new PDO("mysql:host=$db_servername;dbname=$db_dbname", $db_username, $db_password);
-                    // set the PDO error mode to exception
-                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                
-                    // prepare sql and bind parameters
-                    $stmt = $conn->prepare("INSERT INTO user (username, password, status) 
-                    VALUES (:username, :password, :status)");
-                    $stmt->bindParam(':username', $username);
-                    $stmt->bindParam(':password', $password);
-                    $stmt->bindParam(':status', $status);
-                
-                    // insert a row
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                    $status = $_POST['status'];
-                    $stmt->execute();
-                    echo "New records created successfully";
-                    }
-                catch(PDOException $e)
-                    {
-                    echo "Error: " . $e->getMessage();
-                    }
-                $conn = null;
-            }
-        ?>
-         </div>
+                </div>
+            </div>
+            <div class="form-group" style="margin-top:10px;">
+                <div class="row">
+                    <label class="control-label col-sm-2" for="password" style="float:left;">password:</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" id="password" placeholder="Enter password" name="email">
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">     
+                    <div style="float:left;margin-left:15px;">
+                        <div class="checkbox">
+                        <label><input type="checkbox" name="remember"> Remember me</label>
+                        </div>
+                    </div>
+               
+            </div>
+            <div class="form-group">   
+                <div style="float:right;">    
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+            </div>
+        </form>              
+     </div>
  </div>
 </div>
-     </div>
 </div>
-
+</div>
+    <div class="footer">
+    <p>design by hapoly</p>
+    </div>
     </body>
 </html>
