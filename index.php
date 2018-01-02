71625c5a046e074d2fa1523781047968412ee097
mr-exception <drabandi1996@gmail.com>
2018-01-02 21:28:26 +0330

    pagination completed

--- index.php
+++ index.php
@@ -70,14 +70,15 @@ foreach($stmt->fetchAll() as $row) {
   $count = $row['COUNT(*)'];
 }
 if($count > 10){
+  $page_count = ceil($count / 10);
   echo '
     <div class="row" style="text-align: center" >
       <ul class="pagination">
-        <li><a href="#">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
+        <li class="'. ($page==0? 'disabled': '') .'"><a href="?page=0"><i class="fa fa-angle-double-left" aria-hidden="true"></i></a></li>
+        <li class="'. ($page==0? 'disabled': '') .'"><a href="?page='. ($page-1) .'"><i class="fa fa-angle-left" aria-hidden="true"></i></a></li>
+        <li><a href="">'. ($page+1) .'/'. $page_count .'</a></li>
+        <li class="'. ($page==$page_count-1? 'disabled': '') .'"><a href="?page='. ($page+1) .'"><i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+        <li class="'. ($page==$page_count-1? 'disabled': '') .'"><a href="?page='. ($page_count-1) .'"><i class="fa fa-angle-double-right" aria-hidden="true"></i></a></li>
       </ul>
     </div>
   ';
